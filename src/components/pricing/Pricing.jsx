3cdd925c092a57243548fc04142722b825a56d27
mertbayca <103841104+mertbayca@users.noreply.github.com>
2023-04-13 01:32:06 +0300

    Stripe integrated

--- src/components/pricing/Pricing.jsx
+++ src/components/pricing/Pricing.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './Pricing.css'
 import {tick} from '../../assets'
-import StripeCheckout from 'react-stripe-checkout'
+import StripeCheckout from 'react-stripe-checkout';
 
 
 const Pricing = () => {
-  return (
+  
+  const [product, setProduct] = useState({
+    name: 'LotusFocus Subscription',
+    price: 1,
+  });
+
+  const [token, setToken] = useState(null);
+
+  const handleToken = (token) => {
+    console.log(token);
+    setToken(token);
+  };
+
+  const handleSubscription = (price) => {
+    setProduct({ ...product, price });
+  };
+
+    return (
     <div id="pricing" className="price_container">
         <div className="price_zen_gaia">
             <h2>Zen</h2>
@@ -17,6 +34,15 @@ const Pricing = () => {
                 <li><img src={tick} />3 hours of meditation music.</li>
                 <li><img src={tick} />Be involved in the LotusFocus community!</li>
             </ul>
+            <StripeCheckout
+                stripeKey="pk_test_51MwBmCCG7m6MlhJbXzKR63tdMlHk7qbmVUpGUkKCizOV7mfe20HWMbAtUxD1Tf8vhLmeDrCaRTcxEJQTBPKPha7L00Sx7XPhFc"
+                token={handleToken}
+                amount="249"
+                name={product.name}
+                description="Zen subscription plan"
+            >
+                <button onClick={() => handleSubscription(249)}>Subscribe Now</button>
+            </StripeCheckout>
         </div>
         <div className="price_terra">
             <h2>Terra</h2>
@@ -32,6 +58,15 @@ const Pricing = () => {
                 <li><img src={tick} />Stress tracker by LotusFocus.</li>
                 <li><img src={tick} />Exclusive access to 1 masterclass of your choosing.</li>
             </ul>
+            <StripeCheckout
+                stripeKey="pk_test_51MwBmCCG7m6MlhJbXzKR63tdMlHk7qbmVUpGUkKCizOV7mfe20HWMbAtUxD1Tf8vhLmeDrCaRTcxEJQTBPKPha7L00Sx7XPhFc"
+                token={handleToken}
+                amount="849"
+                name={product.name}
+                description="Terra subscription plan"
+            >
+                <button onClick={() => handleSubscription(849)}>Subscribe Now</button>
+            </StripeCheckout>
         </div>
         <div className="price_zen_gaia">
             <h2>Gaia</h2>
@@ -43,6 +78,15 @@ const Pricing = () => {
                 <li><img src={tick} />Personalized coaching sessions.</li>
                 <li><img src={tick} />Advanced tracking tools and support from experts.</li>
             </ul>
+            <StripeCheckout
+                stripeKey="pk_test_51MwBmCCG7m6MlhJbXzKR63tdMlHk7qbmVUpGUkKCizOV7mfe20HWMbAtUxD1Tf8vhLmeDrCaRTcxEJQTBPKPha7L00Sx7XPhFc"
+                token={handleToken}
+                amount="1999"
+                name={product.name}
+                description="Gaia subscription plan"
+            >
+                <button onClick={() => handleSubscription(1999)}>Subscribe Now</button>
+            </StripeCheckout>
         </div>
     </div>
   )
